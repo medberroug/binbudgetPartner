c79613801fa73fe0e7e37144b8e963bbc5dab934
medberroug <medberroug@gmail.com>
2023-11-07 23:37:34 +0100

    Changed Server Port to 3001

--- nuxt.config.js
+++ nuxt.config.js
@@ -46,7 +46,9 @@ export default {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL: 'https://api.binbudget.com',
   },
-
+  server: {
+    port: 3001 // default: 3000
+  },
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   // PWA module configuration
   pwa: {
